e09f77268446189562a1ea05c6cef38bbc78ba49
Blake Fogel <114951798+Blake-Fogel@users.noreply.github.com>
2025-04-27 22:03:27 -0400

    Works now

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const myLibrary = [];
+const myLibrary = new Map();
 const tableBody = document.querySelector('tbody');
+const openDButton = document.querySelector('#open-dialog');
+const dialog = document.querySelector('dialog');
+dialog.addEventListener('close',(event) => {
+    let tempData = new FormData(form);
+    if (form.checkValidity() && dialog.returnValue==='confirm') {
+        addDialogBookToLibrary(tempData);
+    }
+    form.reset();
+});
+const dialogButton = document.querySelector('#submit-dialog');
+const form = document.querySelector('form');
+const stringInputs = document.querySelectorAll('.StringInput');
+stringInputs.forEach(element => {
+    element.addEventListener('input', (event) => {
+        if (element.validity.patternMismatch) {
+            element.setCustomValidity("Enter a string that contains letters/numbers");
+        } else {
+            element.setCustomValidity("");
+        }
+    })
+});
+const numberInput = document.querySelector('.NumberInput');
+numberInput.addEventListener('input', (event) => {
+    if (numberInput.validity.rangeUnderflow || numberInput.validity.rangeOverflow) {
+        numberInput.setCustomValidity("Enter a number 1-10,000");
+    } else {
+        numberInput.setCustomValidity("");
+    }
+})
+document.querySelector('body > button').addEventListener('click',() => {
+    document.querySelector('dialog').showModal();
+});
 function Book(author,title,finishedReading,pageAmount) {
     if (!new.target) {
         throw Error("this is a constructor");
@@ -13,21 +45,53 @@ function Book(author,title,finishedReading,pageAmount) {
 
 function addBookToLibrary(author,title,finishedReading,pageAmount) {
     let book = new Book(author,title,finishedReading,pageAmount);
-    myLibrary.push(book);
+    myLibrary.set(book.id,book);
+    addBookToLibraryTable(book);
+}
+function addDialogBookToLibrary(data) {
+    addBookToLibrary(data.get('Author'),data.get('Title'),(data.get('Finished')!==null ? 'true' : false),data.get('PageCount'));
 }
 function addBookToLibraryTable(book) {
     let row = tableBody.insertRow();
-    for (const property of Object.values(book)) {
-        let td = row.insertCell();
-        td.innerText=property;
+    for (const propertyName in book) {
+        if (propertyName==='finishedReading') {
+            let td = row.insertCell();
+            td.style.verticalAlign = 'center';
+            td.style.textAlign = 'center';
+            let checkBox = document.createElement('input');
+            checkBox.checked = book.finishedReading;
+            checkBox.type = 'checkbox';
+            checkBox.addEventListener('change', (event) => {
+               book.finishedReading = checkBox.checked;
+            });
+            td.appendChild(checkBox);
+        } else {
+            let td = row.insertCell();
+            td.innerText=book[propertyName];   
+        }
     }
-    tableBody.appendChild(row);
+    let td = row.insertCell();
+    let button = document.createElement('button');
+    button.style.display = 'grid';
+    button.style.alignItems = 'center';
+    button.style.justifyItems = 'center';
+    button.innerText = 'Delete';
+    td.appendChild(button);
+    button.addEventListener('click', (event) => {
+        myLibrary.delete(book.id);
+        refreshTable();
+    })
+}
+function clearLibraryTable() {
+    tableBody.innerHTML = '';
 }
 function displayLibrary() {
     for (const book of myLibrary) {
         addBookToLibraryTable(book);
     }
 }
+function refreshTable() {
+    clearLibraryTable();
+    displayLibrary();
+}
 addBookToLibrary('test','testing',true,8153);
-
-displayLibrary();
